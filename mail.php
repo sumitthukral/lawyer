daee58e9078a08c20f5a09feff6a8b47133eca1f
VB Easy <47852318+raj-vbeasy@users.noreply.github.com>
2022-04-16 17:14:42 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -3,16 +3,10 @@
     $email = $_POST['Email'];
     $name = $_POST['Name'];
     $phone= $_POST['Phone'];
-    //$headers = Cc: info@digilucks.com;
-   // $email_to = "info@digilucks.com,rajshakya.ai@gmail.com";
-
-   // $con= mysqli_connect('localhost','root','','test');
-    
-    //$result = $con->query("insert into mail (name,email,mobile)values('$name','$email','$phone')");
-    
-
-    require_once "vendor/autoload.php";
-    
+   $admin='sanajay071@gmail.com';
+    // $con= mysqli_connect('localhost','root','','test');
+   //$result = $con->query("insert into mail (name,email,mobile)values('$name','$email','$phone')");
+   require_once "vendor/autoload.php";
     $mail = new PHPMailer;
     
     //Enable SMTP debugging. 
@@ -20,28 +14,22 @@
     //Set PHPMailer to use SMTP.
     $mail->isSMTP();            
     //Set SMTP host name                          
-    $mail->Host = "digilucks.com";
+    $mail->Host = "smtp.gmail.com";
     //Set this to true if SMTP host requires authentication to send email
     $mail->SMTPAuth = true;                          
     //Provide username and password     
-    //$mail->Username = "info@digilucks.com";                 
-    //$mail->Password = "*T(?{n%FNESh";                           
-    $mail->Username = "sumitthukral9@gmail.com";
-    $mail->Password = "sumitst9#";  
+    $mail->Username = "sumitthukral9@gmail.com";                 
+    $mail->Password = "sumitst9#";                           
     //If SMTP requires TLS encryption then set it
     $mail->SMTPSecure = "tls";                           
     //Set TCP port to connect to 
     $mail->Port = 587;                                   
-    
-    $mail->FromName = "Lead Manager";
-    $mail->From = "info@digilucks.com";
- 
-    $mail->addAddress('info@digilucks.com','admin');
-    
+    $mail->From = "sumitthukral9@gmail.com";
+    $mail->FromName = "Sumit";
+    $mail->addAddress($admin,'admin');
     $mail->isHTML(true);
-    
-    $mail->Subject = 'Lead From Ads/Website';
-    $mail->Body = "Hi, <br/> We have new lead please reply within 24 hours <b><br/><br/>Name : $name<br/>Email: $email<br/>Phone: $phone<br/><br/></b>Thanks<br/>Lead Manager";
+    $mail->Subject = 'Confirmation email';
+    $mail->Body = "<h3>Hi, User</h3> <br/> We have new lead please reply within 24 hours <br/> Name: $name<br/>Email ID: $email<br/>Phone:$phone<br/>";
     $mail->AltBody = "Subscribed Successfully";
     
     if(!$mail->send()) 
@@ -50,7 +38,7 @@
     } 
     else 
     {
-           $mail = new PHPMailer;
+            $mail = new PHPMailer;
     
             //Enable SMTP debugging. 
            // $mail->SMTPDebug = 3;                               
@@ -59,31 +47,26 @@
             //Set SMTP host name                          
             $mail->Host = "smtp.gmail.com";
             //Set this to true if SMTP host requires authentication to send email
-            $mail->SMTPAuth = true;
-//           $mail->Username = "info@digilucks.com";                 
-  //  $mail->Password = "*T(?{n%FNESh";                           
-     $mail->Username = "sumitthukral9@gmail.com";
-     $mail->Password = "sumitst9#";
-
-    //If SMTP requires TLS encryption then set it
-    $mail->SMTPSecure = "tls";                           
-    //Set TCP port to connect to 
-    $mail->Port = 587;                                   
-    
-    $mail->FromName = "Lead Manager";
-    $mail->From = "info@digilucks.com";
+            $mail->SMTPAuth = true;                          
+            //Provide username and password     
+            $mail->Username = "sumitthukral9@gmail.com";                 
+            $mail->Password = "sumitst9#";                           
+            //If SMTP requires TLS encryption then set it
+            $mail->SMTPSecure = "tls";                           
+            //Set TCP port to connect to 
+            $mail->Port = 587;                                   
+            $mail->From = "sumitthukral9@gmail.com";
+            $mail->FromName = "Sumit";
         $mail->addAddress($email,$name);
         $mail->isHTML(true);
         $mail->Subject = 'Thankyou email';
         $mail->Body = "<h3>Hi, $name</h3><br/>Our team will contact you soon";
         $mail->AltBody = "Subscribed Successfully";
         $mail->send();
-
-        //echo "Message has been sent successfully";
         echo"<script>window.location.href='thankyou.html';</script>";
     }
 }
 else{
     echo"<script>window.location.href='index.html';</script>";
 }
-    ?>
+?>
